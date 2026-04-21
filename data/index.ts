6cd0dd718e0e9edee51f3e7b24dfea8ad930c01c
Zeeshan Haider <zeeshandev2005@gmail.com>
2026-04-21 09:50:59 -0700

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -118,9 +118,9 @@ export const projects = [
     id: 6,
     title: "VV Digital",
     des: "Modren Software for the warehouse management.",
-    img: "/black.PNG",
+    img: "/vvdidtal.PNG",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://www.blackstone-media.uk",
+    link: "https://vvdigitals.netlify.app/",
   },
 
 ];
