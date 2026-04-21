cd272f1bbd5edb3d82b4cf8ff38edde78be15208
Zeeshan Haider <zeeshandev2005@gmail.com>
2026-04-21 10:11:44 -0700

    add link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -45,7 +45,7 @@ const Footer = () => {
             <Link
               key={info.id}
                target="_blank"
-              href={"www.linkedin.com/in/zeeshan-haider-mern-0a77aa363"}
+              href={info.link}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <Image
