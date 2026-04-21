b23c150de120c5e64102914572b9562a32e4805b
Zeeshan Haider <zeeshandev2005@gmail.com>
2026-04-21 10:04:06 -0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -44,7 +44,7 @@ const Footer = () => {
           {socialMedia.map((info) => (
             <Link
               key={info.id}
-              href={info.link}
+              href={"www.linkedin.com/in/zeeshan-haider-mern-0a77aa363"}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <Image
